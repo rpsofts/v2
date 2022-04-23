c8246d9eeede1a4c61586e975f30ede2949b8317
Tad <tad@live.it>
2022-04-23 13:45:44 +0800

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# V2Ray generate configuration
+# Download and install V2Ray
+config_path=$PROTOCOL"_ws_tls.json"
+mkdir /tmp/v2ray
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+install -m 644 /tmp/v2ray/geoip.dat /usr/local/bin/geoip.dat
+install -m 644 /tmp/v2ray/geoip-only-cn-private.dat /usr/local/bin/geoip-only-cn-private.dat
+install -m 644 /tmp/v2ray/geosite.dat /usr/local/bin/geosite.dat
+# Remove temporary directory
+rm -rf /tmp/v2ray
+
+# V2Ray new configuration
+install -d /usr/local/etc/v2ray
+envsubst '\$UUID,\$WS_PATH' < $config_path > /usr/local/etc/v2ray/config.json
+# MK TEST FILES
+mkdir /opt/test
+cd /opt/test
+dd if=/dev/zero of=10mb.bin bs=10M count=1
+
+# Run Tor
+nohup tor &
+# Run V2Ray
+# /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json &
+v2ray -config /usr/local/etc/v2ray/config.json &
+# Run nginx
+/bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
